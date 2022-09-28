18e0f8b5742b2bd51ccfcb66724e4510e504b4eb
Eric Song <ericksong91@gmail.com>
2022-09-28 17:16:02 -0400

    commit tree a1458463838659c95ff2a5ffdd3f003ed98d793d

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,32 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(prop) {
+  function getBio(){
+    switch(prop.bio){
+      case undefined:
+        return null;
+        break;
+      case "":
+        return null;
+        break;
+      default:
+        return <p>{prop.bio}</p>
+        break;
+    }
+  }
+
+  // <p>{prop.bio}</p>
+
+  console.log(prop.bio)
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {getBio()}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links {...prop.links}/>
     </div>
   );
 }
